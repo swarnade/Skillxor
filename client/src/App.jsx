c0a334bef78b6c63e83f09b343b502ad0ad737a9
Swarnadeep Saha Poddar <code.isspoddar@gmail.com>
2024-09-11 21:38:23 +0530

    Updated Codebase Due to Merge Error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,11 @@
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
-import { Signup } from "./pages/freelancer/Signup";
-import { Login } from "./pages/freelancer/Login";
-import Applicants from "./pages/Applicants";
-import Projects from "./pages/client/Projects";
-import { ClientSignup } from "./pages/client/ClientSignup";
-import { ClientLogin } from "./pages/client/ClientLogin";
-import Landing from "./pages/Landing";
-
+import { createBrowserRouter, RouterProvider,} from 'react-router-dom'
+import Routes from './routes'
 function App() {
+  const router = createBrowserRouter(Routes)
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="freelancer/signup" element={<Signup />} />
-        <Route path="freelancer/login" element={<Login />} />
-
-        <Route path="client/signup" element={<ClientSignup />} />
-        <Route path="client/login" element={<ClientLogin />} />
-
-        <Route path="/applicants" element={<Applicants />} />
-        <Route path="/client/projects" element={<Projects />} />
-        <Route path="/" element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
 }
-
 export default App;
